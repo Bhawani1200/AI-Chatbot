669c2981e73c1c41859c2075520199b471e27b82
DELL <bhawanilimbu1200@gmail.com>
2026-07-01 17:36:30 +0545

    ai chatbot using react

---
+++ ai-chatbot/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="./chat.webp" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls />
+    </div>
+  );
+};
+
+
+export default App;
